a0021a8cc82752063d9189388a6da71dd7c6a8a5
[martinezk23] <[mkaren2301@gail.com]>
2021-12-30 21:32:33 -0600

    changes to p0 and p1

---
+++ CoffeeShopAppP1/CoffeeShopP1App.App/Customers.cs
@@ -0,0 +1,66 @@
+﻿//12/20/2021 Could make a Person class the Customer could derive from.
+//That will give me the option to add employees as well.
+
+namespace CoffeeShopP1App.App
+{
+    public class Customer
+    {
+        //Version 1- 12/20/21 first attempt to get program to call this
+        //public string nameFirst = "";
+        //public string nameLast = "";
+        ////public string nameFirst { get; private set; }
+        ////public string nameLast { get; private set; }
+        //public Customer(string nameFirst, string nameLast)
+        //{
+        //    this.nameFirst = nameFirst;
+        //    this.nameLast = nameLast;
+        //}
+
+        //public Customer(string v)
+        //{ 
+        //    nameFirst = v;
+        //    nameLast = v;
+
+        //    throw new NotImplementedException();
+        //}
+
+        //Version 2
+        private string firstName;
+        private string lastName;
+
+        //Building a Customer constructor
+        //Version 2 goes with Version 3 of Program
+        public Customer(string first, string last)
+        {
+            firstName = first;
+            lastName = last;
+        }
+        public Customer()
+        {
+            firstName = "Coffee";
+            lastName = "Shop";
+
+        }
+        public void CustomerName()
+        {
+            // //Add excetion here
+            //try
+            //{
+            string customername = null;
+            Console.WriteLine("What's your first name?");
+            customername = Console.ReadLine();
+            Console.WriteLine("What's your last name?");
+            customername = Console.ReadLine();
+            //}
+            //catch(cexception)
+            //{
+            //Console.WriteLine("Invalid name input, please try again")
+            //CustomerName();
+            //return;
+            //}   
+        }
+
+
+
+    }
+}
---
+++ CoffeeShopAppP1/CoffeeShopP1App.App/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace CoffeeShopP1App.App
+{
+    public class Program
+    {
+
+        //public void sqlConnection()
+        //{
+        //    //Add connection to database here
+        //    string connectionString = File.ReadAllText("C:/Users/mkare_wyte20y/Revature/DQL_Database.txt");
+        //    using SqlConnection connection = new(connectionString);
+        //    connection.Open();
+
+        //need to add exception for connection issue
+        //}
+
+        public static void Main(string[] args)
+        {
+
+            ////Reading to database using DataReader
+            //static void HasRows(SqlConnection connection)
+            //{
+            //    using (connection)
+            //    {
+            //        SqlCommand command = new SqlCommand(
+            //          "SELECT Store, CategoryName FROM Categories;",
+            //          connection);
+            //        connection.Open();
+
+            //        SqlDataReader reader = command.ExecuteReader();
+
+            //        if (reader.HasRows)
+            //        {
+            //            while (reader.Read())
+            //            {
+            //                Console.WriteLine("{0}\t{1}", reader.GetInt32(0),
+            //                    reader.GetString(1));
+            //            }
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("If you reading this its connected.");
+            //        }
+            //        reader.Close();
+            //    }
+            //}
+            Console.WriteLine("Hello, CoffeeShop!");
+
+
+
+            //Version 3
+            Customer newCustomer = new Customer();
+            newCustomer.CustomerName();
+
+
+            //version 4 12/21
+            Order newOrder = new Order();
+            newOrder.Ordering();
+
+        }
+
+
+    }
+}
