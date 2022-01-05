b138e6973fe4024bf38ce03bddf0d6efbbd7371f
[martinezk23] <[mkaren2301@gail.com]>
2022-01-04 23:56:28 -0600

    created a seprate API

---
+++ P1CoffeeShopAppAPI/P1CoffeeShopApp.API/Program.cs
@@ -0,0 +1,31 @@
+var builder = WebApplication.CreateBuilder(args);
+
+//connection string
+string connectionString = await File.ReadAllTextAsync("C:/Users/mkare_wyte20y/Revature/DQL_Database.txt");
+builder.Services.AddControllers();
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//singleton
+builder.Services.AddSingleton<IRepository>(sp => new SqlRepository(connectionString));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
