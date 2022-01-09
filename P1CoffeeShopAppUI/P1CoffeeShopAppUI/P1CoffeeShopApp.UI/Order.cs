48c8d0d624688c2406bf6f51c6b0038a5a8fa3be
[martinezk23] <[mkaren2301@gail.com]>
2022-01-09 00:00:11 -0600

    new ui, the other broke.

---
+++ P1CoffeeShopAppUI/P1CoffeeShopAppUI/P1CoffeeShopApp.UI/Order.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P1CoffeeShopApp.UI
+{
+
+    public class Order
+    {
+        //field for adding total coffees together.
+        private decimal orderTotal;
+
+        public Order()
+        {
+            orderTotal = 0;
+        }
+
+
+
+        public void Ordering()
+        {
+            //Logic, Call methods in order of how i need them!
+            welcomeMessage();       //First
+
+
+            //Want to give user total after every order
+            itemSold("Small Coffee", 2m);
+            coffeeCartTotal();
+            itemSold("Medium Coffee", 4m);
+            coffeeCartTotal();
+            itemSold("Large Coffee", 8m);
+            coffeeCartTotal();
+
+            displayGoodbyeMessage();    //Last        
+        }
+
+
+        //Diplay options
+        private void welcomeMessage()
+        {
+            string s = "\n" + "Welcome to the coffee shop." +
+                        "****This is the menu****." +
+                        "  Small Coffee $2.  Medium Coffee $4.  Large Coffee $8. ";
+            s = s.Replace(".", ".\n");
+            //Console.WriteLine("*********************************************");
+            Console.WriteLine(s);
+            //Console.WriteLine("Please press enter to continue");
+            //Console.ReadLine();
+        }
+        //Console.WriteLine("1.Small Coffee@2.Medium Coffee@3Large Coffee");
+        //Console.WriteLine("**********************************************");
+
+
+        private void itemSold(string coffeeSize, decimal price)
+        {
+            Console.WriteLine($"Would you like to order {coffeeSize} for {price:C2} (y/n)");
+            string response = Console.ReadLine().ToUpper();
+            if (response.StartsWith("Y"))
+            {
+
+
+                Console.WriteLine("How many coffees would you like?");
+                string numItems = Console.ReadLine();
+
+
+                try
+                {
+                    int quantity = Convert.ToInt32(numItems);
+                    //This is the calculation
+                    decimal totalCoffee = price * quantity;
+                    orderTotal += totalCoffee;
+
+                    Console.WriteLine($"{quantity} x {coffeeSize}s is {totalCoffee:C2}");
+
+                }
+
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid input, please try again");
+                    itemSold(coffeeSize, price);///Recursion used to ask again 
+
+                    return;
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Invalid input, please try again");
+                    itemSold(coffeeSize, price);///Recursion used to ask again 
+                    return;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Oh you dont want any coffee?");
+            }
+
+
+        }
+        private void coffeeCartTotal()
+        {
+            Console.WriteLine($"Current total is {orderTotal:C2}!");
+        }
+
+
+        private static void displayGoodbyeMessage()
+        {
+
+            Console.WriteLine("Have a wonderul day");
+            Console.WriteLine("Goodbybe");
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ P1CoffeeShopAppUI/P1CoffeeShopAppUI/P1CoffeeShopApp.UI/Program.cs
@@ -0,0 +1,69 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+namespace P1CoffeeShopApp.UI
+{
+    public class Program
+    {
+        //public static readonly HttpClient httpClient = new ();
+        //string connectionString = File.ReadAllText("C:/Users/mkare_wyte20y/Revature/DQL_Database.txt");
+        //need to add exception for connection issue
+        //IRepository repository = new SqlRepository(connectionString); //need to fix
+
+        public static /*async*/ void Main(string[] args)
+        {
+
+            //HttpResponseMessage respone = await HttpClient.GetAsync($"https://localhost:7276/api/");
+
+            //IRepository repository = new SqlRepository(connectionString);//need to fix
+            ////Reading to database using DataReader
+            //static void HasRows(SqlConnection connection)
+            //{
+            //    using (connection)
+            //    {
+            //        SqlCommand command = new SqlCommand(
+            //          "SELECT Store, CategoryName FROM Categories;",
+            //         connection);
+            //        connection.Open();
+
+            //        SqlDataReader reader = command.ExecuteReader();
+
+            //        if (reader.HasRows)
+            //        {
+            //            while (reader.Read())
+            //            {
+            //                Console.WriteLine("{0}\t{1}", reader.GetInt32(0),
+            //                    reader.GetString(1));
+            //            }
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("If you reading this its connected.");
+            //        }
+            //        reader.Close();
+            //    }
+            //}
+            Console.WriteLine("Hello, CoffeeShop!");
+
+
+
+            ////Version 3
+            //Customers newCustomers = new Customers();
+            //newCustomers.customers();
+
+
+            //version 4 12/21
+            Order newOrder = new Order();
+            newOrder.Ordering();
+
+            //Locations newLocations = new Locations();
+            //newLocations.locationId();
+            Console.ReadKey();//
+        }
+
+
+    }
+
+
+}
